13ce6682956f02d36c86fb88aaa28fb7b5b2a618
Aditya-46-Raj <adityaraj21103@gmail.com>
2026-06-18 17:21:21 +0530

    backend setup done

---
+++ backend/src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use(helmet());
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "TalentMind API Running",
+  });
+});
+
+export default app;
